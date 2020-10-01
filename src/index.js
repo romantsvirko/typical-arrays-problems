faa685e123d3ae87fa4a28177695e6e24ff4d04f
romantsvirko <40736898+romantsvirko@users.noreply.github.com>
2020-10-01 23:44:41 +0300

    Update index.js
    init complit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if(!Array.isArray(array) || array == null || array[0] == null){
+       return 0;
+    }
+  return min = Math.min.apply(null, array);
+},
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max (array) {      
+       if(!Array.isArray(array) || array == null || array[0] == null){
+       return 0;
+   }
+ return max = Math.max.apply(null, array);
+},
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array == null || array[0] == null) {
+        return 0;
+} 
+    return avg = array.reduce((a, b) => (a + b)) / array.length;
 }
+
